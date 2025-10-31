779390ebbb067bc686119fee6acd66f71fe38031
rasim hesenov <devbeyt989@gmail.com>
2025-10-31 21:45:02 +0400

    added toggle method

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,25 +1,27 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
+import { toggle } from "../features/todosSlice";
 
 function TodoList() {
-
-	const items = useSelector(state=>state.todos.items)
-	console.log(items)
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.todos.items);
 
   return (
-    	<ul className="todo-list">			
-	        {
-                 items.map(item=>(
-					<li key={item.id} className={item.completed? "completed":""}>
-				<div className="view">
-					<input className="toggle" type="checkbox" />
-					<label>{item.title}</label>
-					<button className="destroy"></button>
-				</div>
-			</li>
-				 ))
-			}
-		</ul>
-  )
+    <ul className="todo-list">
+      {items.map((item) => (
+        <li key={item.id} className={item.completed ? "completed" : ""}>
+          <div className="view">
+            <input
+              className="toggle"
+              type="checkbox"
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
+            <label>{item.title}</label>
+            <button className="destroy"></button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default TodoList
+export default TodoList;
--- src/features/todosSlice.js
+++ src/features/todosSlice.js
@@ -1,24 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = {
-    items:[
-        {id:1,title:"Learn React",completed:true,},
-        {id:2,title:"Learn Ethical Hacking",completed:false,},
-        {id:3,title:"Do Full Body Exercise",completed:false,},
-    ]
-}
+  items: [
+    { id: 1, title: "Learn React", completed: true },
+    { id: 2, title: "Learn Ethical Hacking", completed: false },
+    { id: 3, title: "Do Full Body Exercise", completed: false },
+  ],
+};
 
 export const todosSlice = createSlice({
-    name:'todos',
-    initialState,
-    reducers:{
-        
-        addNewItem: (state,action) =>{
-            state.items.push(action.payload)
-        }
+  name: "todos",
+  initialState,
+  reducers: {
+    addNewItem: (state, action) => {
+      state.items.push(action.payload);
     },
-})
 
-export const {addNewItem} = todosSlice.actions;
+    toggle: (state, action) => {
+      const { id } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      item.completed = !item.completed;
+    },
+  },
+});
+
+export const { addNewItem,toggle } = todosSlice.actions;
 export default todosSlice.reducer;
